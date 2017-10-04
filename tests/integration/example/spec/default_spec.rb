7bdbe5c84caed64a8076593e1d2a2708bdfc5cf8
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-10-04 18:41:55 +0900

    ensure correct permissions
    https://github.com/reallyenglish/ansible-role-cloudstack-init/pull/1#pullrequestreview-66973057

--- tests/integration/example/spec/default_spec.rb
+++ tests/integration/example/spec/default_spec.rb
@@ -15,5 +15,15 @@ context "after provisioning finished" do
       r = current_server.ssh_exec("sudo cat /root/.ssh/authorized_keys")
       expect(r).to match(/#{Regexp.escape("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDaaUGnUNr+mdx2lbSFiL5yGe+dD4pEjInJtQo0pTVcLwIy+dym7wtwHO6aBPQdKTfapkRWEKPlRwArKx1/lPIMiAYtz57NMEdAdbKoCQdaGfAp0gB3Qe9rhxfMpGlsE+7gCRaqzgEBjQIXvh+nD3yVfovWQfCrl5I/zx/AplUSwe8UE02AJ/fnTGqrPt6TbilxwIhr2WYwLIZCXvgTRPF1QM1dK62kFxeEX4FMPRF3cl0glCQnyyofe4iwmjEPm7ch1gIDCw6epavsxG3wa2JurKRIUjgblVxxl6ga4z6ZOgQtmPeKyqe7akln7lzf+lVHuJdiHLPxV4ErlSeqmedX foo@bar")}/)
     end
+
+    it "has /root/.ssh with correct permission" do
+      r = current_server.ssh_exec('export `stat -s /root/.ssh` ; echo -n $st_mode')
+      expect(r).to eq "040700"
+    end
+
+    it "has /root/.ssh/authorized_keys with correct permission" do
+      r = current_server.ssh_exec("export `sudo stat -s /root/.ssh/authorized_keys` ; echo -n $st_mode")
+      expect(r).to eq "0100600"
+    end
   end
 end
