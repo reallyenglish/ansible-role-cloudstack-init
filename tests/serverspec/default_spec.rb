a4717e8f1f9544d92cca528ee88a8163cff513be
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-10-12 13:38:26 +0700

    [feature] Support OpenBSD (#2)
    * support OpenBSD
    
    * add openbsd_61
    
    * fix Vagrantfile
    
    * rubocopfy
    
    * fix wrong regexp
    
    * rename
    
    * backward-compatibility
    
    * increment the IP addresses
    
    not sure but i am guessing overrapped addresses causes the failure in
    Jenkins
    
    * increment in inventories as well
    
    * update all IP addresses
    
    * integrate all tests into one
    
    * rename to default and remove unused tests
    
    * rubocopfy
    
    * remove unused template
    
    * remove unnecessary regexp
    
    https://github.com/reallyenglish/ansible-role-cloudstack-init/pull/2#discussion_r144194396
    
    * use jinja2
    
    https://github.com/reallyenglish/ansible-role-cloudstack-init/pull/2#discussion_r144194788

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,16 +1,25 @@
 require "spec_helper"
 require "serverspec"
 
+describe file("/usr/local/sbin/cloudsshkey") do
+  it { should exist }
+  it { should be_file }
+  it { should be_mode 755 }
+  it { should be_owned_by "root" }
+  it { should be_grouped_into "wheel" }
+end
+
 case os[:family]
-when "freebsd"
-  describe file("/usr/local/sbin/cloudsshkey") do
+when "openbsd"
+  describe file("/etc/rc.local") do
     it { should exist }
     it { should be_file }
-    it { should be_mode 755 }
+    it { should be_mode 644 }
     it { should be_owned_by "root" }
     it { should be_grouped_into "wheel" }
+    its(:content) { should match(/^#{Regexp.escape("/usr/local/sbin/cloudsshkey")}$/) }
   end
-
+when "freebsd"
   describe file("/usr/local/etc/rc.d/cloudsshkey") do
     it { should exist }
     it { should be_file }
